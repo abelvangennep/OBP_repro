edd25421735809e36d02a527d6133fea3558d948
Arsol Drifa <arsdola@gmail.com>
2022-01-19 16:46:45 +0100

    database added

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,98 @@
 from django.db import models
 
 # Create your models here.
+class Restaurants(models.Model):
+    lat = models.FloatField()
+    lon = models.FloatField()
+    pizza_parallel = models.PositiveIntegerField(default=0)
+    pizza_production_minutes = models.PositiveIntegerField(default=0)
+    shoarma_parallel = models.PositiveIntegerField(default=0)
+    shoarma_production_minutes = models.PositiveIntegerField(default=0)
+    burger_parallel = models.PositiveIntegerField(default=0)
+    burger_production_minutes = models.PositiveIntegerField(default=0)
+    fries_parallel = models.PositiveIntegerField(default=0)
+    fries_production_minutes = models.PositiveIntegerField(default=0)
+    snack_parallel = models.PositiveIntegerField(default=0)
+    snack_production_minutes = models.PositiveIntegerField(default=0)
+    french_parallel = models.PositiveIntegerField(default=0)
+    french_production_minutes = models.PositiveIntegerField(default=0)
+    spanish_parallel = models.PositiveIntegerField(default=0)
+    spanish_production_minutes = models.PositiveIntegerField(default=0)
+    italian_parallel = models.PositiveIntegerField(default=0)
+    italian_production_minutes = models.PositiveIntegerField(default=0)
+    greek_parallel = models.PositiveIntegerField(default=0)
+    greek_production_minutes = models.PositiveIntegerField(default=0)
+    german_parallel = models.PositiveIntegerField(default=0)
+    german_production_minutes = models.PositiveIntegerField(default=0)
+    african_parallel = models.PositiveIntegerField(default=0)
+    african_production_minutes = models.PositiveIntegerField(default=0)
+    mexican_parallel = models.PositiveIntegerField(default=0)
+    mexican_production_minutes = models.PositiveIntegerField(default=0)
+    argentine_parallel = models.PositiveIntegerField(default=0)
+    argentine_production_minutes = models.PositiveIntegerField(default=0)
+    indian_parallel = models.PositiveIntegerField(default=0)
+    indian_production_minutes = models.PositiveIntegerField(default=0)
+    chinese_parallel = models.PositiveIntegerField(default=0)
+    chinese_production_minutes = models.PositiveIntegerField(default=0)
+    sushi_parallel = models.PositiveIntegerField(default=0)
+    sushi_production_minutes = models.PositiveIntegerField(default=0)
+    luxe_parallel = models.PositiveIntegerField(default=0)
+    luxe_production_minutes = models.PositiveIntegerField(default=0)
+    mon_open = models.DateTimeField(default=None)
+    mon_close = models.DateTimeField(default=None)
+    tue_open = models.DateTimeField(default=None)
+    tue_close = models.DateTimeField(default=None)
+    wed_open = models.DateTimeField(default=None)
+    wed_close = models.DateTimeField(default=None)
+    thu_open = models.DateTimeField(default=None)
+    thu_close = models.DateTimeField(default=None)
+    fri_open = models.DateTimeField(default=None)
+    fri_close = models.DateTimeField(default=None)
+    sat_open = models.DateTimeField(default=None)
+    sat_close = models.DateTimeField(default=None)
+    sun_open = models.DateTimeField(default=None)
+    sun_close = models.DateTimeField(default=None)
+
+
+
+class Vehicles(models.Model):
+    capacity = models.PositiveIntegerField(default=0)
+    fixed_cost = models.PositiveIntegerField(default=0)
+    hour_costs = models.PositiveIntegerField(default=0)
+    km_costs = models.FloatField(default=0)
+    allowed_on_highspeed_roads = models.BinaryField(default=0)
+
+class Orders(models.Model):
+    STATES = (
+        ('N', 'None'),
+        ('R', 'Restaurants'),
+        ('D', 'Deliverer'),
+    )
+
+    lat = models.FloatField()
+    lon = models.FloatField()
+    order_time = models.DateTimeField()
+    pizza_amount = models.PositiveIntegerField()
+    shoarma_amount = models.PositiveIntegerField()
+    burger_amount = models.PositiveIntegerField()
+    fries_amount = models.PositiveIntegerField()
+    snack_amount = models.PositiveIntegerField()
+    french_amount = models.PositiveIntegerField()
+    spanish_amount = models.PositiveIntegerField()
+    italian_amount = models.PositiveIntegerField()
+    greek_amount = models.PositiveIntegerField()
+    german_amount = models.PositiveIntegerField()
+    african_amount = models.PositiveIntegerField()
+    mexican_amount = models.PositiveIntegerField()
+    argentine_amount = models.PositiveIntegerField()
+    indian_amount = models.PositiveIntegerField()
+    chinese_amount = models.PositiveIntegerField()
+    sushi_amount = models.PositiveIntegerField()
+    luxe_amount = models.PositiveIntegerField()
+    state = models.CharField(max_length = 1, choices = STATES, blank=True, null=True)
+    selected_restaurant = models.ForeignKey(Restaurants, on_delete=models.CASCADE, blank=True, null=True)
+    selected_deliverer = models.ForeignKey(Vehicles, on_delete=models.CASCADE, blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.id}"
+
