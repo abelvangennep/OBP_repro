5f7b7165f88b49c1822f5823a41c990d138635a6
abelvangennep <abel1998@gmail.com>
2022-01-20 16:24:35 +0100

    Worked on restaurant app

--- orders/views.py
+++ orders/views.py
@@ -5,14 +5,16 @@ from .models import Orders
 def orders(request):
     active_order = Orders.objects.filter(state__isnull=True).order_by('id')[0]
 
-    type_of_meals = ['pizza_amount']
-    basket = {}
+    type_of_meals = ["pizza's   "]
+    basket = []
     for meal in type_of_meals:
         if active_order.pizza_amount > 0:
-            basket['Pizza amount'] = active_order.pizza_amount
+            basket.append((meal, active_order.pizza_amount))
+    print(basket)
     context = {
         "active_order":active_order,
-        "basket":basket,
+        "basket": basket,
+        "order_status": "Not yet subjected to Restaurant",
     }
 
 
