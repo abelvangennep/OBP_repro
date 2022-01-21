5a11a67e21746e6c839f508870acada0325e0895
abelvangennep <abel1998@gmail.com>
2022-01-21 15:05:35 +0100

    working on side and topbar

--- orders/urls.py
+++ orders/urls.py
@@ -3,7 +3,8 @@ from . import views
 from orders.dash_apps.test_app import template
 
 urlpatterns = [
-    path('', views.orders, name='orders'),
+    path('', views.restaurants, name='restaurants'),
     path('deliverer', views.deliverer, name='deliverer'),
-    path('restaurant_selection/<int:restaurant_id>', views.restaurant_selection, name='restaurant_selection')
+    path('restaurant_selection/<int:restaurant_id>', views.restaurant_selection, name='restaurant_selection'),
+    path('search_view', views.search_view, name='search_view')
 ]
--- orders/views.py
+++ orders/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from .models import Orders,Restaurants
 
 # Create your views here.
-def orders(request):
+def restaurants(request):
 
     active_order = Orders.objects.filter(state__isnull=True).order_by('id')[0]
 
@@ -17,7 +17,7 @@ def orders(request):
         "basket":basket,
     }
 
-    return render(request, 'orders/orders.html', context)
+    return render(request, 'orders/restaurants.html', context)
 
 
 def deliverer(request):
@@ -46,4 +46,17 @@ def restaurant_selection(request, restaurant_id):
     active_order.selected_restaurant_id = restaurant
     active_order.save(update_fields=['selected_restaurant_id', 'state'])
     
+    return redirect('orders')
+
+def search_view(request):
+    if request.method == "POST":
+        searched = request.POST["main_search_bar"]
+        try:
+            searched_order = Orders.objects.get(id=searched)
+            return render(request, 'orders/orders.html', {"active_order":searched_order})
+        except:
+            searched_order = Orders.objects.get(id=searched)
+            max = Orders.objects.all().order_by("-id")[0]
+            min = Orders.objects.all().order_by("id")[0]
+            return render(request, 'orders/orders.html', {"max":max, "min":min})
     return redirect('orders')
