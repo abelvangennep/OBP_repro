fb1cfb0f85381d66ef37ce5147bcd14837741ea6
abelvangennep <abel1998@gmail.com>
2022-02-01 10:37:05 +0100

    Update urls.py

--- orders/urls.py
+++ orders/urls.py
@@ -5,6 +5,7 @@ from orders.dash_apps.test_app import simpleexample
 urlpatterns = [
     path('', views.restaurants, name='restaurants'),
     path('deliverer', views.deliverer, name='deliverer'),
+    path('merge', views.merge, name='merge'),
     path('restaurant_selection/<int:option_id>', views.restaurant_selection, name='restaurant_selection'),
     path('merge/<int:deliverer_id>', views.merge, name='merge'),
     path('non_merge/<int:option_id>', views.non_merge, name='Non_merge'),
